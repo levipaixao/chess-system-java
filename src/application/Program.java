b2bb4b799d062cfeec4a7ce3a050a30e39e6a2b6
Levi Paixão <levipaixao32@gmail.com>
2026-04-11 16:18:05 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
             try {
 
                 UI.clearScreen(); // serve para limpar o terminal
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(input);
